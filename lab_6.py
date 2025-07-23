abb2142c23e3a795ada54c27d60f05c41d160258
Rob Culbertson <robertculbertsonv@gmail.com>
2025-07-23 00:07:03 -0400

    Kowabunga baby

---
+++ lab_6.py
@@ -0,0 +1,38 @@
+import flask
+from flask import render_template, request, redirect, url_for
+from datetime import datetime
+app = flask.Flask(__name__)
+"""
+Flask App for Lab 6 - CYOP 300
+Written by Rob Culbertson
+"""
+@app.route('/')
+def index():
+    """
+    index landing page
+    """
+    return flask.render_template('index.html')
+@app.route('/<name>')
+def dog(name):
+    """
+    Pulls datetime and personalizes the landing page for the name entered
+    :param name: User's name
+    """
+    now = datetime.now()
+    fm_now = now.strftime("%H:%M:%S")
+    fm_now_date = now.strftime("%d/%m/%Y")
+    name_up = name.capitalize()
+    return render_template('dog.html', name=name_up, time = fm_now, date=fm_now_date)
+
+@app.route('/process_name', methods=['POST'])
+def process_name():
+    """
+    This processes the form submission and redirects to a dynamic URL
+    """
+    if request.method == 'POST':
+        name=request.form['name'].strip()
+        return redirect(url_for('dog', name=name)) #Redirects the url to the dynamic entry of the user
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
